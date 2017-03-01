c11374980ab16c92aba15a5f853c8f04ca69a4f8
Akond Rahman <akond.rahman.buet@gmail.com>
2017-02-28 19:22:27 -0500

    tokenization stuff

--- src/main_tokenization.py
+++ src/main_tokenization.py
@@ -8,11 +8,13 @@ from sklearn.feature_extraction.text import CountVectorizer
 
 
 def getTokensForTokenization(datasetParam):
+   completeLabels    = []
    completeCorpus    = [] ## a list of lists with tokens from defected and non defected files
    with open(datasetParam, 'rU') as f:
      reader_ = csv.reader(f)
      next(reader_, None)
      for row in reader_:
+       defectStatus = row[20]
        fileToRead   = row[1]
        fileContentAsStr = utility.giveCommentFreeFileContent(fileToRead)
        #print "!"*75
@@ -21,21 +23,26 @@ def getTokensForTokenization(datasetParam):
        #print len(filtered_str_from_one_file)
        #print "="*75
        completeCorpus.append(filtered_str_from_one_file)
-
-   return completeCorpus
+       ### after getting the text , getthe labels
+       completeLabels.append(defectStatus)
+   return completeCorpus, completeLabels
 
 
 def executeTokenization(tokenTuple):
   iac_vectorizer = CountVectorizer(min_df=1)
-
+  all_features  = iac_vectorizer.fit_transform(tokenTuple)
+  ###print fitted_model
+  ## get feature names
+  feature_names = iac_vectorizer.get_feature_names()
+  print "The words are:", feature_names
 
 print "Started at", utility.giveTimeStamp()
 print "-"*125
 dataset_file="/Users/akond/Documents/AkondOneDrive/OneDrive/IaC-Defect-Prediction-Project/dataset/SYNTHETIC_MOZ_FULL_DATASET.csv"
 # dataset_file="/Users/akond/Documents/AkondOneDrive/OneDrive/IaC-Defect-Prediction-Project/dataset/SYNTHETIC_WIKI_FULL_DATASET.csv"
 ### this is a lsit of strings ... each string si pre-processed , and correpsonds to all tokens of a file
-unfilteredTokensFromFile = getTokensForTokenization(dataset_file)
-print unfilteredTokensFromFile
+unfilteredTokensFromFile, defectLabels = getTokensForTokenization(dataset_file)
+#print "tokens:{}, labels:{}".format(unfilteredTokensFromFile, defectLabels)
 executeTokenization(unfilteredTokensFromFile)
 print "Ended at", utility.giveTimeStamp()
 print "-"*125
